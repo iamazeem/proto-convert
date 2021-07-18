49708652adf723536cf0710802e19bb549f01a43
Azeem Sajid <azeem.sajid@gmail.com>
2021-07-18 08:08:35 +0500

    Update tests.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -11,7 +11,7 @@ diff test.bin test-out.bin && rm test-out.bin
 echo
 echo "Test # 2: Binary to JSON conversion (test.proto)"
 proto-convert -m b2j -p test.proto -t test.Message -i test.bin -o test-out.json
-jq < test-out.json
+cat test-out.json | jq .
 diff test.json test-out.json && rm test-out.json
 echo
 echo "Test # 3: JSON to Binary conversion (log.proto)"
@@ -20,7 +20,7 @@ diff log.bin log-out.bin && rm log-out.bin
 echo
 echo "Test # 4: Binary to JSON conversion (log.proto)"
 proto-convert -m b2j -p log.proto -t service.logging.Log -i log.bin -o log-out.json
-jq < log-out.json
+cat log-out.json | jq .
 diff log.json log-out.json && rm log-out.json
 echo
 echo "--- [DONE] ---"
