fdc438a4750a77e1c86c37903d71a94fa52e8c82
Azeem Sajid <azeem.sajid@gmail.com>
2021-07-27 12:30:36 +0500

    Enable debug flag for CI tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -6,20 +6,20 @@ cd test
 echo "Running tests"
 echo
 echo "Test # 1: JSON to Binary conversion (test.proto)"
-proto-convert -m j2b -p test.proto -t test.Message -i test.json -o test-out.bin
+proto-convert -m j2b -p test.proto -t test.Message -i test.json -o test-out.bin -d
 diff test.bin test-out.bin && rm test-out.bin
 echo
 echo "Test # 2: Binary to JSON conversion (test.proto)"
-proto-convert -m b2j -p test.proto -t test.Message -i test.bin -o test-out.json
+proto-convert -m b2j -p test.proto -t test.Message -i test.bin -o test-out.json -d
 cat test-out.json | jq .
 diff test.json test-out.json && rm test-out.json
 echo
 echo "Test # 3: JSON to Binary conversion (log.proto)"
-proto-convert -m j2b -p log.proto -t service.logging.Log -i log.json -o log-out.bin
+proto-convert -m j2b -p log.proto -t service.logging.Log -i log.json -o log-out.bin -d
 diff log.bin log-out.bin && rm log-out.bin
 echo
 echo "Test # 4: Binary to JSON conversion (log.proto)"
-proto-convert -m b2j -p log.proto -t service.logging.Log -i log.bin -o log-out.json
+proto-convert -m b2j -p log.proto -t service.logging.Log -i log.bin -o log-out.json -d
 cat log-out.json | jq .
 diff log.json log-out.json && rm log-out.json
 echo
